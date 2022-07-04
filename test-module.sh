a35611436142764beef5390d27c2cc7ed45d010d
cds-amal <cds.sudama@gmail.com>
2022-07-04 06:38:50 -0400

    shellcheck scripts

--- test-module.sh
+++ test-module.sh
@@ -2,39 +2,39 @@
 
 pkg="$1"
 
-mkdir $pkg
-cd $pkg
+mkdir "$pkg" && cd "$pkg" > /dev/null || exit
 
 # create manifest
-npm init -y
+npm init -y > /dev/null
 
 # write test script
 cat << EOF > index.js 
 try {
   require("@truffle/PKG");
-  console.log("Success: PKG OK!");
+  console.log("Success: @truffle/PKG OK!");
 } catch (err) {
   console.log(err);
-  console.log("Fail: PKG NOK!");
+  console.log("Fail: @truffle/PKG NOK!");
 }
 
 EOF
 sed -i "s:PKG:$pkg:g" index.js
 
 START_TIME=$(date +%s)
+echo "Probing @truffle/${pkg}"
 
 # install package
 npm i --registry http://localhost:4873 "@truffle/$pkg" > npm.out 2>&1
 
-cd -
+cd - > /dev/null || exit
 
 # run script
 GREP_RESULT=$(grep -C 1 "@truffle/${pkg}" "${pkg}/npm.out")
-NPM_RESULT=$(node $pkg) 
+NPM_RESULT=$(node "$pkg") 
 
-# perform a task
+# calculate time
 END_TIME=$(date +%s)
+ELAPSED=$(( END_TIME - START_TIME ))
 
-# elapsed time with second resolution
-elapsed=$(( END_TIME - START_TIME ))
-printf "${GREP_RESULT}\n${NPM_RESULT} ${elapsed} seconds\n\n" >> test-report.out
+# record summary
+printf "%s\n%s %s seconds\n\n" "${GREP_RESULT}" "${NPM_RESULT}" "${ELAPSED}" >> test-report.out
--- test.sh
+++ test.sh
@@ -1,3 +1,9 @@
 #!/usr/bin/env bash
 
-cat truffle-modules | xargs --max-procs 16 -n 1 ./test-module.sh
+# for each line in truffle-modules
+#    invoke test-module.sh line
+# 
+# xargs
+#  --max-args 1: use at most 1 argument per invocation
+#  --max-procs: use maximum number of processors available
+xargs --max-procs 0 --max-args 1 ./test-module.sh < truffle-modules
